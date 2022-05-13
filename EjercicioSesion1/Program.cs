aefbc26809087526853ff03526c644b7d2c58117
duezt <klkrdcf@gmail.com>
2022-05-13 00:48:13 -0400

    Agregar archivos de proyecto.

---
+++ EjercicioSesion1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Ejercicio 1
+Console.WriteLine("Cuales es tu nombre?");
+string name = Console.ReadLine();
+Console.WriteLine("Hola un placer conocerte " + name);
+
+
+//Ejercicio 2
+
+Console.WriteLine("Que hora es?");
+string time = Console.ReadLine();
+Console.WriteLine("son las " + time);
